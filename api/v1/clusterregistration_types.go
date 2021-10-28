fda58408e1d85e6820b6f166fcac08e44ec9a4ca
Bich Le <leb@platform9.com>
2021-10-27 23:07:12 -0700

    initial commit

---
+++ api/v1/clusterregistration_types.go
@@ -0,0 +1,64 @@
+/*
+Copyright 2021.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// ClusterRegistrationSpec defines the desired state of ClusterRegistration
+type ClusterRegistrationSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+
+	// Foo is an example field of ClusterRegistration. Edit clusterregistration_types.go to remove/update
+	Foo string `json:"foo,omitempty"`
+}
+
+// ClusterRegistrationStatus defines the observed state of ClusterRegistration
+type ClusterRegistrationStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+}
+
+//+kubebuilder:object:root=true
+//+kubebuilder:subresource:status
+
+// ClusterRegistration is the Schema for the clusterregistrations API
+type ClusterRegistration struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   ClusterRegistrationSpec   `json:"spec,omitempty"`
+	Status ClusterRegistrationStatus `json:"status,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+
+// ClusterRegistrationList contains a list of ClusterRegistration
+type ClusterRegistrationList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []ClusterRegistration `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&ClusterRegistration{}, &ClusterRegistrationList{})
+}
