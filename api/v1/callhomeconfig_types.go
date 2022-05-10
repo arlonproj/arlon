a6e3129a9b472ad4336839d7f033c131f3f1645f
Bich Le <leb@platform9.com>
2022-05-10 13:37:28 -0700

    add documentation to the CallHomeConfigSpec type

--- api/v1/callhomeconfig_types.go
+++ api/v1/callhomeconfig_types.go
@@ -23,14 +23,27 @@ import (
 // EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
 // NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
 
-// CallHomeConfigSpec defines the desired state of CallHomeConfig
+// CallHomeConfigSpec defines the desired state of CallHomeConfig.
+// The resource's status becomes 'complete' when a target secret named TargetSecretName
+// is successfully created in the TargetNamespace of the workload cluster
+// identified by ManagementClusterUrl and authenticated via the kubeconfig contained
+// in the secret named KubeconfigSecretName in the management cluster.
+// The target secret will contain a kubeconfig generated from the token associated
+// with the service account named ServiceAccountName in the management cluster.
 type CallHomeConfigSpec struct {
+	// Name of autoscaler service account name in the management cluster
 	ServiceAccountName      string `json:"serviceAccountName"`      //
+	// Name of secret containing kubeconfig for workload cluster
 	KubeconfigSecretName    string `json:"kubeconfigSecretName"`    //
+	// Name of key inside of the secret that holds the kubeconfig
 	KubeconfigSecretKeyName string `json:"kubeconfigSecretKeyName"` //
+	// Name of namespace inside workload cluster in which to create new kubeconfig secret
 	TargetNamespace         string `json:"targetNamespace"`         //
+	// Name of secret inside workload cluster
 	TargetSecretName        string `json:"targetSecretName"`        //
+	// Name of key holding the kubeconfig inside of the target secret
 	TargetSecretKeyName     string `json:"targetSecretKeyName"`     //
+	// The URL of the management cluster
 	ManagementClusterUrl    string `json:"managementClusterUrl"`    //
 }
 
