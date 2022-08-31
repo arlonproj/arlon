96c1fa9c19b5c92284e081d72821a40b685800f9
Bich Le <leb@platform9.com>
2022-08-31 12:56:47 -0700

    (WIP) testing/ubuntu_devel_prereqs.sh testing/ubuntu_testbed_prereqs.sh

---
+++ testing/ubuntu_devel_prereqs.sh
@@ -0,0 +1,22 @@
+set -e
+set -o pipefail
+
+cd
+mkdir -p ~/downloads
+pushd downloads
+
+# Uninstall go package just in case. Idempotent
+sudo apt -y update
+sudo apt -y purge golang-go
+
+wget https://go.dev/dl/go1.19.linux-amd64.tar.gz
+tar xf go1.19.linux-amd64.tar.gz
+
+sudo ln -s /home/ubuntu/downloads/go/bin/go /usr/local/bin/go
+sudo ln -s /home/ubuntu/downloads/go/bin/gofmt /usr/local/bin/gofmt
+
+popd
+mkdir -p devel
+pushd devel
+git clone https://github.com/arlonproj/arlon.git
+
