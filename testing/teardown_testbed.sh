6fb5684b19bb0f055ce9a35c206b7ca571daa290
Bich Le <leb@platform9.com>
2022-08-31 09:27:28 -0700

    teardown: ignore errors

--- testing/teardown_testbed.sh
+++ testing/teardown_testbed.sh
@@ -1,4 +1,4 @@
 
-kind delete cluster --name kind-arlon-testbed
-docker stop arlon-testbed-gitserver
+kind delete cluster --name kind-arlon-testbed || true
+docker stop arlon-testbed-gitserver || true
 sudo rm -rf /tmp/arlon-testbed-git*
