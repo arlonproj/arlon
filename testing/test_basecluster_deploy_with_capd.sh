0844b20744d0e3d89cc551fe8a38c60af7996020
Bich Le <leb@platform9.com>
2022-08-31 15:33:49 -0700

    test_basecluster_deploy_with_capd.sh enhancements

--- testing/test_basecluster_deploy_with_capd.sh
+++ testing/test_basecluster_deploy_with_capd.sh
@@ -1,9 +1,11 @@
 
 set -e
 
+repourl=http://172.17.0.1:8188/myrepo.git
 repodir=/tmp/arlon-testbed-git-clone/myrepo
-baseclusterdir=${repodir}/capd-test
+baseclusterdir=${repodir}/baseclusters/capd-test
 manifestfile=capd-capi-quickstart-withclusternamelabelremoved.yaml
+
 if ! [ -f ${baseclusterdir}/capd-capi-quickstart-withclusternamelabelremoved.yaml ]; then
     echo adding basecluster directory
     mkdir -p ${baseclusterdir}
@@ -16,3 +18,15 @@ if ! [ -f ${baseclusterdir}/capd-capi-quickstart-withclusternamelabelremoved.yam
     popd
 fi
 
+if arlon basecluster validategit --repo-url ${repourl} --repo-path capd-test 2> /tmp/stdout.txt; then
+    echo basecluster/capd-test already prepped
+else
+    if ! grep "Error: kustomization.yaml is missing" /tmp/stdout.txt &> /dev/null ; then
+        echo unexpected output from validategit, check /tmp/stdout.txt
+        exit 1
+    fi
+    if ! arlon basecluster preparegit --repo-url ${repourl} --repo-path capd-test; then
+        echo preparegit failed
+        exit 1
+    fi
+fi
