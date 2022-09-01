4eaa5f4c61cabbea0282d8855a1258d573e8256e
Bich Le <leb@platform9.com>
2022-08-31 17:25:49 -0700

    deploy capd-1 cluster and verify results

--- testing/test_basecluster_deploy_with_capd.sh
+++ testing/test_basecluster_deploy_with_capd.sh
@@ -1,5 +1,19 @@
 
 set -e
+# $1 = expression
+# $2 = sleep period
+# $3 = iterations
+function wait_until()
+{
+    for i in `seq 1 $3`
+    do
+        eval $1 && return 0
+        echo "Waiting for \"$1\" to evaluate to true ..."
+        sleep $2
+    done
+    echo Timed out waiting for \"$1\"
+    return 1
+}
 
 repourl=http://172.17.0.1:8188/myrepo.git
 repodir=/tmp/arlon-testbed-git-clone/myrepo
@@ -31,3 +45,16 @@ else
         exit 1
     fi
 fi
+
+arlon cluster create --cluster-name capd-1 --repo-url http://172.17.0.1:8188/myrepo.git --repo-path baseclusters/capd-test --profile dynamic-2-calico
+
+echo '------- waiting for control plane to become ready ---------'
+wait_until 'set -o pipefail; clusterctl -n capd-1 describe cluster capd-1-capi-quickstart|grep ControlPlane|grep True > /dev/null' 10 30
+
+echo '------- waiting for machinedeployment to become ready (happens when calico is deployed) ---------'
+wait_until 'set -o pipefail; clusterctl -n capd-1 describe cluster capd-1-capi-quickstart|grep MachineDeployment|grep True > /dev/null' 10 60
+
+echo '------- waiting for guestbook to become healthy ---------'
+wait_until 'set -o pipefail; argocd app list|grep capd-1-guestbook-dynamic|grep Healthy > /dev/null' 10 30
+
+echo '--- test ok ---'
