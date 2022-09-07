bcc34b254de34855c56eab4c78746df109bd7a92
Rohit Rajak <39493074+Rohitrajak1807@users.noreply.github.com>
2022-09-07 20:43:59 +0530

    Fix profile delete bug. (#196)
    * attempt delete bugfix
    
    * attempt delete bugfix
    
    * remove commented
    
    * review comment
    
    * review comments
    
    * review comments

--- pkg/profile/delete.go
+++ pkg/profile/delete.go
@@ -3,11 +3,13 @@ package profile
 import (
 	"context"
 	"fmt"
+	v1 "github.com/arlonproj/arlon/api/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
-func Delete(
+func DeleteLegacy(
 	kubeClient *kubernetes.Clientset,
 	ns string,
 	profileName string,
@@ -19,4 +21,16 @@ func Delete(
 		return fmt.Errorf("failed to delete profile: %s", err)
 	}
 	return nil
+}
+
+func Delete(cli client.Client, ns, profileName string) error {
+	ctx := context.Background()
+	prof := v1.Profile{
+		TypeMeta: metav1.TypeMeta{},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      profileName,
+			Namespace: ns,
+		},
+	}
+	return cli.Delete(ctx, &prof, &client.DeleteOptions{})
 }
