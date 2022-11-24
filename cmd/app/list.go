a6f5fb658e65e61513737a7db3d95cd73af78320
Bich Le <leb@platform9.com>
2022-11-23 16:20:51 -0800

    display app-profiles in 'app list' command output

--- cmd/app/list.go
+++ cmd/app/list.go
@@ -3,8 +3,12 @@ package app
 import (
 	"fmt"
 	"github.com/arlonproj/arlon/pkg/app"
+	"github.com/arlonproj/arlon/pkg/appprofile"
 	"github.com/spf13/cobra"
+	restclient "k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
+	"os"
+	"text/tabwriter"
 )
 
 import "github.com/argoproj/argo-cd/v2/util/cli"
@@ -21,10 +25,40 @@ func listAppsCommand() *cobra.Command {
 			if err != nil {
 				return fmt.Errorf("failed to get k8s client config: %s", err)
 			}
-			return app.ListToStdout(config, ns)
+			return listToStdout(config, ns)
 		},
 	}
 	clientConfig = cli.AddKubectlFlagsToCmd(command)
 	command.Flags().StringVar(&ns, "ns", "argocd", "the argo-cd namespace")
 	return command
+}
+
+func listToStdout(config *restclient.Config, ns string) error {
+	apps, err := app.List(config, ns)
+	if err != nil {
+		return fmt.Errorf("failed to list apps: %s", err)
+	}
+	profiles, err := appprofile.List(config, "arlon")
+	if err != nil {
+		return fmt.Errorf("failed to list application profiles: %s", err)
+	}
+	appToProf := make(map[string][]string)
+	for _, prof := range profiles {
+		for _, appName := range prof.Spec.AppNames {
+			appToProf[appName] = append(appToProf[appName], prof.Name)
+		}
+	}
+	w := tabwriter.NewWriter(os.Stdout, 0, 0, 2, ' ', 0)
+	_, _ = fmt.Fprintf(w, "NAME\tREPO\tPATH\tREVISION\tAPP_PROFILES\n")
+	for _, app := range apps {
+		_, _ = fmt.Fprintf(w, "%s\t%s\t%s\t%s\t%s\n",
+			app.Name,
+			app.Spec.Template.Spec.Source.RepoURL,
+			app.Spec.Template.Spec.Source.Path,
+			app.Spec.Template.Spec.Source.TargetRevision,
+			appToProf[app.Name],
+		)
+	}
+	_ = w.Flush()
+	return nil
 }
--- pkg/app/app.go
+++ pkg/app/app.go
@@ -5,13 +5,10 @@ import (
 	"fmt"
 	appset "github.com/argoproj/argo-cd/v2/pkg/apis/applicationset/v1alpha1"
 	"github.com/arlonproj/arlon/pkg/ctrlruntimeclient"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/apimachinery/pkg/selection"
 	restclient "k8s.io/client-go/rest"
-	"os"
 	"sigs.k8s.io/controller-runtime/pkg/client"
-	"text/tabwriter"
 )
 
 const ProfileLabelKey = "arlon.io/profile"
@@ -36,35 +33,4 @@ func List(config *restclient.Config, ns string) (apslist []appset.ApplicationSet
 		return nil, fmt.Errorf("failed to list applicationsets: %s", err)
 	}
 	return asl.Items, nil
-}
-
-func ListToStdout(config *restclient.Config, ns string) error {
-	apps, err := List(config, ns)
-	if err != nil {
-		return fmt.Errorf("failed to list apps: %s", err)
-	}
-	w := tabwriter.NewWriter(os.Stdout, 0, 0, 2, ' ', 0)
-	_, _ = fmt.Fprintf(w, "NAME\tREPO\tPATH\tREVISION\tPROFILES\n")
-	for _, app := range apps {
-		profiles := "(none)"
-		if len(app.Spec.Generators) == 1 &&
-			app.Spec.Generators[0].Clusters != nil &&
-			len(app.Spec.Generators[0].Clusters.Selector.MatchExpressions) >= 1 {
-			if app.Spec.Generators[0].Clusters.Selector.MatchExpressions[0].Key != ProfileLabelKey ||
-				app.Spec.Generators[0].Clusters.Selector.MatchExpressions[0].Operator != metav1.LabelSelectorOpIn {
-				profiles = "(invalid data)"
-			} else {
-				profiles = fmt.Sprintf("%s", app.Spec.Generators[0].Clusters.Selector.MatchExpressions[0].Values)
-			}
-		}
-		_, _ = fmt.Fprintf(w, "%s\t%s\t%s\t%s\t%s\n",
-			app.Name,
-			app.Spec.Template.Spec.Source.RepoURL,
-			app.Spec.Template.Spec.Source.Path,
-			app.Spec.Template.Spec.Source.TargetRevision,
-			profiles,
-		)
-	}
-	_ = w.Flush()
-	return nil
 }
