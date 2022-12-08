ae171a27180886ff45cd41c5d6abe5ca17344156
Jayanth Reddy <jaya@platform9.com>
2022-12-08 20:59:12 +0530

    using os to openfile

--- pkg/gitutils/manifests.go
+++ pkg/gitutils/manifests.go
@@ -4,6 +4,7 @@ import (
 	"embed"
 	"fmt"
 	"io"
+	"os"
 	"path"
 	"strings"
 
@@ -53,41 +54,27 @@ func CopyManifests(wt *gogit.Worktree, fs embed.FS, root string, mgmtPath string
 
 // -----------------------------------------------------------------------------
 
-func CopyPatchManifests(wt *gogit.Worktree, fs embed.FS, root string, mgmtPath string) error {
+func CopyPatchManifests(wt *gogit.Worktree, fs embed.FS, filePath string, mgmtPath string) error {
 	log := log.GetLogger()
-	items, err := fs.ReadDir(root)
+	src, err := os.OpenFile(filePath, os.O_RDONLY, os.ModePerm)
 	if err != nil {
-		return fmt.Errorf("failed to read embedded directory: %s", err)
+		return fmt.Errorf("failed to open embedded file %s: %s", filePath, err)
 	}
-	for _, item := range items {
-		filePath := path.Join(root, item.Name())
-		if item.IsDir() {
-			if err := CopyManifests(wt, fs, filePath, mgmtPath); err != nil {
-				return err
-			}
-		} else {
-			src, err := fs.Open(filePath)
-			if err != nil {
-				return fmt.Errorf("failed to open embedded file %s: %s", filePath, err)
-			}
-			// remove manifests/ prefix
-			components := strings.Split(filePath, "/")
-			dstPath := path.Join(components[1:]...)
-			dstPath = path.Join(mgmtPath, dstPath)
-			dst, err := wt.Filesystem.Create(dstPath)
-			if err != nil {
-				_ = src.Close()
-				return fmt.Errorf("failed to create destination file %s: %s", dstPath, err)
-			}
-			_, err = io.Copy(dst, src)
-			_ = src.Close()
-			_ = dst.Close()
-			if err != nil {
-				return fmt.Errorf("failed to copy embedded file: %s", err)
-			}
-			log.V(1).Info("copied embedded file", "destination", dstPath)
-		}
-
+	// remove manifests/ prefix
+	components := strings.Split(filePath, "/")
+	dstPath := path.Join(components[len(components)-1])
+	dstPath = path.Join(mgmtPath, dstPath)
+	dst, err := wt.Filesystem.Create(dstPath)
+	if err != nil {
+		_ = src.Close()
+		return fmt.Errorf("failed to create destination file %s: %s", dstPath, err)
 	}
+	_, err = io.Copy(dst, src)
+	_ = src.Close()
+	_ = dst.Close()
+	if err != nil {
+		return fmt.Errorf("failed to copy embedded file: %s", err)
+	}
+	log.V(1).Info("copied embedded file", "destination", dstPath)
 	return nil
 }
