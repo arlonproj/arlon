1995022368938f60eb93e78ccac57ffae469f7c1
Jayanth Reddy <jaya@platform9.com>
2022-12-08 16:02:00 +0530

    Experimenting git repo addition

--- cmd/cluster/create.go
+++ cmd/cluster/create.go
@@ -3,19 +3,11 @@ package cluster
 import (
 	_ "embed"
 	"fmt"
-	"os"
 
-	"github.com/argoproj/argo-cd/v2/pkg/apis/application/v1alpha1"
 	"github.com/argoproj/argo-cd/v2/util/cli"
-	arlonv1 "github.com/arlonproj/arlon/api/v1"
-	"github.com/arlonproj/arlon/pkg/argocd"
-	bcl "github.com/arlonproj/arlon/pkg/basecluster"
 	"github.com/arlonproj/arlon/pkg/cluster"
 	"github.com/arlonproj/arlon/pkg/gitrepo"
-	"github.com/arlonproj/arlon/pkg/profile"
 	"github.com/spf13/cobra"
-	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/apimachinery/pkg/runtime/serializer/json"
 	"k8s.io/client-go/tools/clientcmd"
 )
 
@@ -29,8 +21,10 @@ func createClusterCommand() *cobra.Command {
 	var clusterRepoUrl string
 	var repoAlias string
 	var clusterRepoRevision string
+	var repoBranch string
 	var clusterRepoPath string
 	var clusterName string
+	var overRides string
 	var outputYaml bool
 	var profileName string
 	command := &cobra.Command{
@@ -45,86 +39,87 @@ func createClusterCommand() *cobra.Command {
 					return err
 				}
 			}
-			conn, appIf := argocd.NewArgocdClientOrDie("").NewApplicationClientOrDie()
-			defer conn.Close()
+			//			conn, appIf := argocd.NewArgocdClientOrDie("").NewApplicationClientOrDie()
+			//			defer conn.Close()
 			config, err := clientConfig.ClientConfig()
 			if err != nil {
 				return fmt.Errorf("failed to get k8s client config: %s", err)
 			}
-			_, creds, err := argocd.GetKubeclientAndRepoCreds(config, argocdNs, clusterRepoUrl)
+			//			_, creds, err := argocd.GetKubeclientAndRepoCreds(config, argocdNs, clusterRepoUrl)
 			if err != nil {
 				return fmt.Errorf("failed to get repository credentials: %s", err)
 			}
-			createInArgoCd := !outputYaml
-			baseClusterName, err := bcl.ValidateGitDir(creds,
-				clusterRepoUrl, clusterRepoRevision, clusterRepoPath)
-			if err != nil {
-				return fmt.Errorf("failed to validate base cluster: %s", err)
-			}
-			var prof *arlonv1.Profile
-			if profileName != "" {
-				prof, err = profile.Get(config, profileName, arlonNs)
-				if err != nil {
-					return fmt.Errorf("failed to get profile: %s", err)
-				}
-				if prof.Spec.RepoUrl == "" {
-					return fmt.Errorf("profile %s is not dynamic",
-						profileName)
-				}
-			}
-			// Create "arlon app" for cluster
-			arlonApp, err := cluster.Create(appIf, config, argocdNs, arlonNs,
-				clusterName, baseClusterName, arlonRepoUrl, arlonRepoRevision,
-				arlonRepoPath, "",
-				nil, createInArgoCd, config.Host)
-			if err != nil {
-				return fmt.Errorf("failed to create arlon app: %s", err)
-			}
-			// Create "cluster app" for cluster
-			clusterApp, err := cluster.CreateClusterApp(appIf, argocdNs,
-				clusterName, baseClusterName, clusterRepoUrl, clusterRepoRevision,
-				clusterRepoPath, createInArgoCd)
-			if err != nil {
-				return fmt.Errorf("failed to create cluster app: %s", err)
-			}
-			// Create "profile app" for cluster if necessary
-			var profileApp *v1alpha1.Application
-			if prof != nil {
-				profileAppName := fmt.Sprintf("%s-profile-%s", clusterName, prof.Name)
-				profileApp, err = cluster.CreateProfileApp(profileAppName,
-					appIf, argocdNs, clusterName, prof, createInArgoCd)
-				if err != nil {
-					return fmt.Errorf("failed to create profile app: %s", err)
-				}
-			}
-			if outputYaml {
-				scheme := runtime.NewScheme()
-				if err := v1alpha1.AddToScheme(scheme); err != nil {
-					return fmt.Errorf("failed to add scheme: %s", err)
-				}
-				s := json.NewSerializerWithOptions(json.DefaultMetaFactory,
-					scheme, scheme, json.SerializerOptions{
-						Yaml:   true,
-						Pretty: true,
-						Strict: false,
-					})
-				err = s.Encode(arlonApp, os.Stdout)
-				if err != nil {
-					return fmt.Errorf("failed to encode arlon app: %s", err)
-				}
-				fmt.Println("---")
-				err = s.Encode(clusterApp, os.Stdout)
-				if err != nil {
-					return fmt.Errorf("failed to encode cluster app: %s", err)
-				}
-				if profileApp != nil {
-					fmt.Println("---")
-					err = s.Encode(profileApp, os.Stdout)
-					if err != nil {
-						return fmt.Errorf("failed to encode profile app: %s", err)
-					}
-				}
-			}
+			cluster.CreatePatchDir(config, clusterName, clusterRepoUrl, argocdNs, clusterRepoPath, repoBranch)
+			// createInArgoCd := !outputYaml
+			// baseClusterName, err := bcl.ValidateGitDir(creds,
+			// 	clusterRepoUrl, clusterRepoRevision, clusterRepoPath)
+			// if err != nil {
+			// 	return fmt.Errorf("failed to validate base cluster: %s", err)
+			// }
+			// var prof *arlonv1.Profile
+			// if profileName != "" {
+			// 	prof, err = profile.Get(config, profileName, arlonNs)
+			// 	if err != nil {
+			// 		return fmt.Errorf("failed to get profile: %s", err)
+			// 	}
+			// 	if prof.Spec.RepoUrl == "" {
+			// 		return fmt.Errorf("profile %s is not dynamic",
+			// 			profileName)
+			// 	}
+			// }
+			// // Create "arlon app" for cluster
+			// arlonApp, err := cluster.Create(appIf, config, argocdNs, arlonNs,
+			// 	clusterName, baseClusterName, arlonRepoUrl, arlonRepoRevision,
+			// 	arlonRepoPath, "",
+			// 	nil, createInArgoCd, config.Host)
+			// if err != nil {
+			// 	return fmt.Errorf("failed to create arlon app: %s", err)
+			// }
+			// // Create "cluster app" for cluster
+			// clusterApp, err := cluster.CreateClusterApp(appIf, argocdNs,
+			// 	clusterName, baseClusterName, clusterRepoUrl, clusterRepoRevision,
+			// 	clusterRepoPath, createInArgoCd)
+			// if err != nil {
+			// 	return fmt.Errorf("failed to create cluster app: %s", err)
+			// }
+			// // Create "profile app" for cluster if necessary
+			// var profileApp *v1alpha1.Application
+			// if prof != nil {
+			// 	profileAppName := fmt.Sprintf("%s-profile-%s", clusterName, prof.Name)
+			// 	profileApp, err = cluster.CreateProfileApp(profileAppName,
+			// 		appIf, argocdNs, clusterName, prof, createInArgoCd)
+			// 	if err != nil {
+			// 		return fmt.Errorf("failed to create profile app: %s", err)
+			// 	}
+			// }
+			// if outputYaml {
+			// 	scheme := runtime.NewScheme()
+			// 	if err := v1alpha1.AddToScheme(scheme); err != nil {
+			// 		return fmt.Errorf("failed to add scheme: %s", err)
+			// 	}
+			// 	s := json.NewSerializerWithOptions(json.DefaultMetaFactory,
+			// 		scheme, scheme, json.SerializerOptions{
+			// 			Yaml:   true,
+			// 			Pretty: true,
+			// 			Strict: false,
+			// 		})
+			// 	err = s.Encode(arlonApp, os.Stdout)
+			// 	if err != nil {
+			// 		return fmt.Errorf("failed to encode arlon app: %s", err)
+			// 	}
+			// 	fmt.Println("---")
+			// 	err = s.Encode(clusterApp, os.Stdout)
+			// 	if err != nil {
+			// 		return fmt.Errorf("failed to encode cluster app: %s", err)
+			// 	}
+			// 	if profileApp != nil {
+			// 		fmt.Println("---")
+			// 		err = s.Encode(profileApp, os.Stdout)
+			// 		if err != nil {
+			// 			return fmt.Errorf("failed to encode profile app: %s", err)
+			// 		}
+			// 	}
+			// }
 			return nil
 		},
 	}
@@ -137,8 +132,10 @@ func createClusterCommand() *cobra.Command {
 	command.Flags().StringVar(&clusterRepoUrl, "repo-url", "", "the git repository url for cluster template")
 	command.Flags().StringVar(&repoAlias, "repo-alias", gitrepo.RepoDefaultCtx, "git repository alias to use")
 	command.Flags().StringVar(&clusterRepoRevision, "repo-revision", "main", "the git revision for cluster template")
+	command.Flags().StringVar(&repoBranch, "repo-branch", "main", "the git branch")
 	command.Flags().StringVar(&clusterRepoPath, "repo-path", "", "the git repository path for cluster template")
 	command.Flags().StringVar(&clusterName, "cluster-name", "", "the cluster name")
+	command.Flags().StringVar(&overRides, "overrides", "", "path to the corresponding patch file to the cluster")
 	command.Flags().BoolVar(&outputYaml, "output-yaml", false, "output root applications YAML instead of deploying to ArgoCD")
 	command.Flags().StringVar(&profileName, "profile", "", "profile name (if specified, must refer to dynamic profile)")
 	command.MarkFlagRequired("cluster-name")
--- pkg/cluster/git.go
+++ pkg/cluster/git.go
@@ -3,6 +3,9 @@ package cluster
 import (
 	"embed"
 	"fmt"
+	"path"
+	"text/template"
+
 	arlonv1 "github.com/arlonproj/arlon/api/v1"
 	"github.com/arlonproj/arlon/pkg/argocd"
 	"github.com/arlonproj/arlon/pkg/bundle"
@@ -10,8 +13,6 @@ import (
 	logpkg "github.com/arlonproj/arlon/pkg/log"
 	"github.com/arlonproj/arlon/pkg/profile"
 	gogit "github.com/go-git/go-git/v5"
-	"path"
-	"text/template"
 )
 
 //go:embed manifests/*
@@ -168,3 +169,57 @@ func ProcessDynamicProfile(
 }
 
 // -----------------------------------------------------------------------------
+func DeployPatchToGit(
+	creds *argocd.RepoCreds,
+	argocdNs string,
+	clusterName string,
+	repoUrl string,
+	repoBranch string,
+	basePath string,
+) error {
+	log := logpkg.GetLogger()
+	repo, tmpDir, auth, err := argocd.CloneRepo(creds, repoUrl, repoBranch)
+	if err != nil {
+		return fmt.Errorf("failed to clone repo: %s", err)
+	}
+	clusterPath := clusterPathFromBasePath(basePath, clusterName)
+	wt, err := repo.Worktree()
+	if err != nil {
+		return fmt.Errorf("failed to get repo worktree: %s", err)
+	}
+	// remove old data if directory exists, we'll regenerate everything
+	fileInfo, err := wt.Filesystem.Lstat(clusterPath)
+	if err == nil {
+		if !fileInfo.IsDir() {
+			return fmt.Errorf("unexpected file type for %s", clusterPath)
+		}
+		_, err = wt.Remove(clusterPath)
+		if err != nil {
+			return fmt.Errorf("failed to recursively delete cluster directory: %s", err)
+		}
+	}
+	err = gitutils.CopyManifests(wt, content, ".", clusterPath)
+	if err != nil {
+		return fmt.Errorf("failed to copy embedded content: %s", err)
+	}
+
+	changed, err := gitutils.CommitChanges(tmpDir, wt, "deploy arlon cluster "+clusterPath)
+	if err != nil {
+		return fmt.Errorf("failed to commit changes: %s", err)
+	}
+	if !changed {
+		log.Info("no changed files, skipping commit & push")
+		return nil
+	}
+	err = repo.Push(&gogit.PushOptions{
+		RemoteName: gogit.DefaultRemoteName,
+		Auth:       auth,
+		Progress:   nil,
+		CABundle:   nil,
+	})
+	if err != nil {
+		return fmt.Errorf("failed to push to remote repository: %s", err)
+	}
+	log.V(1).Info("successfully pushed working tree", "tmpDir", tmpDir)
+	return nil
+}
