894a0d4913cff8a47340e8b46d71a8ada1ac9d27
Trilok Khairnar <trilok@platform9.com>
2023-01-28 00:57:57 +0530

    Fixing build error after rebasing main / the PR earlier showed a conflict

--- pkg/controller/controller.go
+++ pkg/controller/controller.go
@@ -13,8 +13,6 @@ import (
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
 	"k8s.io/client-go/rest"
-	restclient "k8s.io/client-go/rest"
-	"os"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
@@ -214,7 +212,7 @@ func StartAppProfileController(argocdConfigPath string, metricsAddr string, prob
 }
 
 func StartClusterController(
-	config *restclient.Config,
+	config *rest.Config,
 	argocdConfigPath string,
 	metricsAddr string,
 	probeAddr string,
